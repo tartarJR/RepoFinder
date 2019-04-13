28429433821fc28dc430e5ad2a266aa483b6263f
tartarJR <mustafaogun.ozturk@gmail.com>
2019-04-13 21:56:43 +0300

    Add PrimaryLanguage property to Repository data class

--- app/src/main/java/com/tatar/repofinder/Repository.kt
+++ app/src/main/java/com/tatar/repofinder/Repository.kt
@@ -1,3 +1,9 @@
 package com.tatar.repofinder
 
-data class Repository(var name: String, var description: String, var forkCount: Int, var owner: Owner)
+data class Repository(
+    var name: String,
+    var description: String,
+    var forkCount: Int,
+    var owner: Owner,
+    var primaryLanguage: PrimaryLanguage
+)
