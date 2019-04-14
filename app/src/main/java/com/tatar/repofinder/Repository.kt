c517316dd78d4fa12050974375449dc97ab1c612
tartarJR <mustafaogun.ozturk@gmail.com>
2019-04-14 23:01:37 +0300

    Refactor Repository data class
    Made some properties nullable

--- app/src/main/java/com/tatar/repofinder/Repository.kt
+++ app/src/main/java/com/tatar/repofinder/Repository.kt
@@ -2,9 +2,9 @@ package com.tatar.repofinder
 
 data class Repository(
     var name: String,
-    var description: String,
+    var description: String?,
     var forkCount: Int,
     var ownerName: String,
     var ownerAvatarUrl: String,
-    var primaryLanguage: String
+    var primaryLanguage: String?
 )
