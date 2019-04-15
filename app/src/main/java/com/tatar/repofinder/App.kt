f807a55576f8bf4c6cd8756e3cfd1abab49e6bd0
tartarJR <mustafaogun.ozturk@gmail.com>
2019-04-15 16:21:41 +0300

    Add dependency tree setup to App class

--- app/src/main/java/com/tatar/repofinder/App.kt
+++ app/src/main/java/com/tatar/repofinder/App.kt
@@ -1,7 +1,27 @@
 package com.tatar.repofinder
 
 import android.app.Application
+import com.tatar.repofinder.di.app.component.AppComponent
+import com.tatar.repofinder.di.app.component.DaggerAppComponent
+import com.tatar.repofinder.di.app.module.AppModule
 
 class App : Application() {
+
+    private lateinit var appComponent: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+
+        instance = this
+        appComponent = DaggerAppComponent.builder().appModule(AppModule(this)).build()
+    }
+
+    fun appComponent(): AppComponent {
+        return appComponent
+    }
+
+    companion object {
+        lateinit var instance: App private set
+    }
 
 }
