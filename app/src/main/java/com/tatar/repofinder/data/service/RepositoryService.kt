6d9ea47d3c6e804493bf436393c831e205a68753
tartarJR <mustafaogun.ozturk@gmail.com>
2019-04-16 01:15:15 +0300

    Refactor RepositoryService
    Using constructor injection for ApolloClient now

--- app/src/main/java/com/tatar/repofinder/data/service/RepositoryService.kt
+++ app/src/main/java/com/tatar/repofinder/data/service/RepositoryService.kt
@@ -2,27 +2,25 @@ package com.tatar.repofinder.data.service
 
 import GetRepositoriesByQualifiersAndKeywordsQuery
 import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.exception.ApolloException
-import com.tatar.repofinder.App
 import com.tatar.repofinder.data.model.Repository
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.error
 import type.SearchType
 
-class RepositoryService : AnkoLogger {
-
-    private var apolloClient = App.instance.appComponent().apolloClient()
+class RepositoryService(private val apolloClient: ApolloClient) : AnkoLogger {
 
     fun getRepositoriesByQualifiersAndKeywords(searchParam: String, onFinishedListener: OnFinishedListener) {
 
-        val queryCall = GetRepositoriesByQualifiersAndKeywordsQuery
+        val searchQuery = GetRepositoriesByQualifiersAndKeywordsQuery
             .builder()
             .query(searchParam)
             .first(25)
             .type(SearchType.REPOSITORY)
             .build()
 
-        apolloClient.query(queryCall)
+        apolloClient.query(searchQuery)
             .enqueue(object : ApolloCall.Callback<GetRepositoriesByQualifiersAndKeywordsQuery.Data>() {
                 override fun onFailure(e: ApolloException) {
                     onFinishedListener.onError()
--- app/src/main/java/com/tatar/repofinder/di/app/module/RepositoryServiceModule.kt
+++ app/src/main/java/com/tatar/repofinder/di/app/module/RepositoryServiceModule.kt
@@ -1,17 +1,18 @@
 package com.tatar.repofinder.di.app.module
 
+import com.apollographql.apollo.ApolloClient
 import com.tatar.repofinder.data.service.RepositoryService
 import com.tatar.repofinder.di.app.scope.PerApp
 import dagger.Module
 import dagger.Provides
 
-@Module
+@Module(includes = [ApolloModule::class])
 object RepositoryServiceModule {
 
     @JvmStatic
     @PerApp
     @Provides
-    fun repositoryService(): RepositoryService {
-        return RepositoryService()
+    fun repositoryService(apolloClient: ApolloClient): RepositoryService {
+        return RepositoryService(apolloClient)
     }
 }
