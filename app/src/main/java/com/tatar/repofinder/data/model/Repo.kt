b9c37e35f4ab3e2c79a68b36120092451828e19a
tartarJR <mustafaogun.ozturk@gmail.com>
2019-04-17 10:30:18 +0300

    Refactor Repository data class name to  Repo

--- app/src/main/java/com/tatar/repofinder/data/model/Repo.kt
+++ app/src/main/java/com/tatar/repofinder/data/model/Repo.kt
@@ -1,10 +1,10 @@
 package com.tatar.repofinder.data.model
 
-data class Repository(
+data class Repo(
     var name: String,
-    var description: String,
+    var description: String?,
     var forkCount: Int,
     var ownerName: String,
     var ownerAvatarUrl: String,
-    var primaryLanguage: String
+    var primaryLanguage: String?
 )
