a2c7afde10591d333e43d4d3b0741e1e7c1c1c50
tartarJR <mustafaogun.ozturk@gmail.com>
2019-04-17 15:03:25 +0300

    Add concrete presenter implementation for detail

--- app/src/main/java/com/tatar/repofinder/ui/detail/DetailContract.kt
+++ app/src/main/java/com/tatar/repofinder/ui/detail/DetailContract.kt
@@ -1,15 +1,18 @@
 package com.tatar.repofinder.ui.detail
 
+import com.tatar.repofinder.data.model.Subscriber
 import com.tatar.repofinder.ui.base.BaseContract.BasePresenter
 import com.tatar.repofinder.ui.base.BaseContract.BaseView
 
 interface DetailContract {
 
     interface DetailView : BaseView {
-        fun displayRepositoryDetails()
+        fun displayRepositoryDetails(subscribers: ArrayList<Subscriber>)
+        fun displayNoSubscriberFoundMessage()
+        fun hideSubscriberList()
     }
 
     interface DetailPresenter : BasePresenter<DetailView?> {
-        fun getRepositoryDetails()
+        fun getRepositoryDetails(repoName: String, repoOwnerName: String)
     }
 }
--- app/src/main/java/com/tatar/repofinder/ui/detail/DetailPresenterImpl.kt
+++ app/src/main/java/com/tatar/repofinder/ui/detail/DetailPresenterImpl.kt
@@ -1,24 +1,35 @@
 package com.tatar.repofinder.ui.detail
 
-import com.tatar.repofinder.data.model.Repository
-import com.tatar.repofinder.data.service.RepositoryService
+import com.tatar.repofinder.data.model.Subscriber
+import com.tatar.repofinder.data.service.RepoService
+import com.tatar.repofinder.data.service.RepoServiceResponse
+import com.tatar.repofinder.data.service.RepoServiceListener
+import com.tatar.repofinder.ui.base.BaseContract
 import com.tatar.repofinder.ui.detail.DetailContract.DetailPresenter
 import com.tatar.repofinder.ui.detail.DetailContract.DetailView
-import com.tatar.repofinder.ui.search.SearchPresenterImpl
 import com.tatar.repofinder.util.ConnectionManager
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.error
 
 class DetailPresenterImpl(
-    private val repositoryService: RepositoryService,
+    private val repoService: RepoService,
     private val connectionManager: ConnectionManager
-) : DetailPresenter, RepositoryService.OnFinishedListener {
+) : DetailPresenter, RepoServiceListener<Subscriber> {
 
-    private val logger = AnkoLogger(SearchPresenterImpl::class.java)
+    private val logger = AnkoLogger(DetailPresenterImpl::class.java)
 
     private var detailView: DetailView? = null
 
-    override fun getRepositoryDetails() {
-
+    override fun getRepositoryDetails(repoName: String, repoOwnerName: String) {
+        if (this.detailView != null) {
+            if (!connectionManager.hasInternetConnection()) {
+                detailView?.displayNoInternetWarning()
+            } else {
+                repoService.getRepositoryDetails(repoName, repoOwnerName, this)
+            }
+        } else {
+            logger.error(BaseContract.BasePresenter.DETACHED_VIEW_ERROR)
+        }
     }
 
     override fun attach(view: DetailView?) {
@@ -29,12 +40,32 @@ class DetailPresenterImpl(
         this.detailView = null
     }
 
-    override fun onResponse(repositoryList: ArrayList<Repository>) {
+    override fun onResponse(repoServiceResponse: RepoServiceResponse<Subscriber>) {
+        if (this.detailView != null) {
+            detailView?.hideProgressBar()
 
+            val subscribers = repoServiceResponse.items
+
+            if (subscribers.isEmpty()) {
+                detailView?.displayNoSubscriberFoundMessage()
+            } else {
+                detailView?.hideStatusTv()
+                detailView?.displayRepositoryDetails(subscribers)
+            }
+        } else {
+            logger.error(BaseContract.BasePresenter.DETACHED_VIEW_ERROR)
+        }
     }
 
     override fun onError() {
-
+        if (this.detailView != null) {
+            detailView?.hideSubscriberList()
+            detailView?.hideProgressBar()
+            detailView?.displayErrorMessage()
+            detailView?.showStatusTv()
+        } else {
+            logger.error(BaseContract.BasePresenter.DETACHED_VIEW_ERROR)
+        }
     }
 
 }
